23c485f38f63261668959cbff846317c55065aa8
Kristjan Rang <mail@rang.ee>
2020-05-21 01:00:09 +0300

    Bump to node 14.3

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = "12.16.2"
+    version = "14.3.0"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodebin/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
